c1207a3bfc9886b8c5e66c9bc0c2c11c908f204c
nikottokaisha <150411147+nikottokaisha@users.noreply.github.com>
2023-11-22 15:54:04 +0900

    Add files via upload

--- rpg/javascript.js
+++ rpg/javascript.js
@@ -2,10 +2,17 @@
 var canvas, g;
 var maptip;
 var map;
-var spReimu, spMarisa, spCirno, spKyoufu,spChirno2,spSaikyo;
 var spriteList;
 var imgMarisa;
 var mae = 0;
+var sensei = 1000000000000000000000000000;
+var metal = 25;
+var metal0 = 21;
+var spReimu, spMarisa, spCirno, spKyoufu,spChirno2,spSaikyo,spOjisan,spTuyoi,spMetal,spMetal2;
+var hyoujisuru = "";
+var oji0 = 21;
+var oji1 = 19;
+var keikenhyouji2 = "経験値." + keiken + "/" + keikena;
 var level = 1;
 var mae0 = 0;
 var mae1 = 1;
@@ -13,6 +20,78 @@ var mae2 = 2;
 var mae3 = 3;
 var okane = 0;
 var maxhp = 118;
+var map2 = [
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+];
+map1 = [
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,0,0,0,0,0,3],
+  [3,0,0,0,0,0,3,3,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,3],
+  [0,0,0,0,0,3,3,3,3,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,3,3,3,3,3,3,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,3,3,3,0,3,3,0,0,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [3,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+  [3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+];
 var hp = 118;
 var name0 = "ニコットマン";
 var namer = name0
@@ -24,6 +103,9 @@ var saishoniageru = 15;
 var atc = 47;
 var def = 45;
 var speed = 45;
+var iti;
+var metal1 = 17;
+var metal2 = 18;
 var set0 = 0;
 var set1 = 2;
 var keikena = 155;
@@ -76,6 +158,7 @@ setInterval(function(arg1, arg2) {
   document.getElementById("def").innerText = "防御:"+ player.def;
   document.getElementById("okane").innerText = "お金:"+ okane;
   document.getElementById("name").innerText = "名前:"+ namer;
+  keikenhyouji = keikenhyouji2;
   keikenhyouji = "経験値." + keiken + "/" + keikena;
     if (keiken > keikena - 1) { 
       keiken = keiken - keikena;
@@ -86,7 +169,6 @@ setInterval(function(arg1, arg2) {
       player.atc = level * 2 + player.atc
       player.speed = level * 1 + player.speed
       level = level + 1;
-      keikenhyouji = "経験値." + keiken + "/" + keikena;
       leveldesu = "レベル." + level
       console.log("Levelは" + level);
       console.log("経験値は" + keiken);
@@ -113,10 +195,10 @@ function init() {
     [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,3],
     [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,0,0,0,0,0,0,3],
     [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,0,0,0,0,0,3],
-    [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,3],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [3,0,0,0,0,0,3,3,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,3],
+    [0,0,0,0,0,3,3,3,3,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,3,3,3,3,3,3,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,3,3,3,0,3,3,0,0,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [3,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,3],
     [3,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
     [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
@@ -146,17 +228,25 @@ function init() {
 
   // スプライトの初期化
   spReimu = new Sprite();
+  spMetal = new Sprite();
   spMarisa = new Sprite();
   spKyoufu = new Sprite();
   spCirno = new Sprite();
   spCirno2 = new Sprite();
   spSaikyo = new Sprite();
+  spOjisan = new Sprite();
+  spTuyoi = new Sprite();
+  spMetal2 = new Sprite();
   spriteList = [];
   spriteList.push(spMarisa);
+  spriteList.push(spMetal);
   spriteList.push(spKyoufu);
   spriteList.push(spCirno);
   spriteList.push(spCirno2);
   spriteList.push(spSaikyo);
+  spriteList.push(spOjisan);
+  spriteList.push(spTuyoi);
+  spriteList.push(spMetal2);
 
   // 初期位置
   spReimu.posx = 12;
@@ -171,19 +261,35 @@ function init() {
   spCirno.posy = 8;
   spCirno2.posx = 26;
   spCirno2.posy = 21;
+  spOjisan.posx = 21;
+  spOjisan.posy = 19;
+  spTuyoi.posx = 35;
+  spTuyoi.posy = 35;
+  spMetal.posx = 7;
+  spMetal.posy = 14;
+  spMetal2.posx = 35;
+  spMetal2.posy = 35;
 
   // キャラ画像読込
   for (var i = 0; i < 2; i++) {
     spReimu.image[i] = new Image();
     spMarisa.image[i] = new Image();
     spKyoufu.image[i] = new Image();
+    spOjisan.image[i] = new Image();
     spCirno.image[i] = new Image();
     spCirno2.image[i] = new Image();
     spSaikyo.image[i] = new Image();
+    spTuyoi.image[i] = new Image();
+    spMetal.image[i] = new Image();
+    spMetal2.image[i] = new Image();
     spReimu.image[i].src = "anime/stop0.png";
     spMarisa.image[i].src = "./nikottoa.png";
     spKyoufu.image[i].src = "./nikottoa.png";
+    spTuyoi.image[i].src = "anime/metalnikotto.png";
     spSaikyo.image[i].src = "anime/stop4.png";
+    spMetal.image[i].src = "anime/hairu.png";
+    spMetal2.image[i].src = "anime/hairu.png";
+    spOjisan.image[i].src = "anime/kowaiojisan.png";
     spCirno.image[i].src = "./cirno" + i + ".png";
     spCirno2.image[i].src = "./cirno" + i + ".png";
   }
@@ -191,6 +297,10 @@ function init() {
   imgMarisa.src = "./nikottoa.png";
   imgSaikyo = new Image();
   imgSaikyo.src = "anime/stop4.png";
+  imgOjisan = new Image();
+  imgOjisan.src = "anime/kowaoji0l.png";
+  imgMetaln = new Image();
+  imgMetaln.src = "anime/metalnikotto0.png";
 
   // その他
   scrollX = 0;
@@ -233,19 +343,77 @@ function inputCheck() {
       } else if (currentEvent.type == 1) {
         // 魔理沙
         scene = Scenes.Battle;
-        currentEnemy = new BattleCharacter("恐怖マンB", 35, 130, 17.5, 0, imgMarisa, this.shokin = 35,ageru = 15,this.keikenplus = 95,this.modoru = 10,this.modoru2 = 9)
+        currentEnemy = new BattleCharacter("恐怖マンB", 35, 130, 17.5, 0, imgMarisa, this.shokin = 35,ageru = 15,this.keikenplus = 95,this.modoru = 10,this.modoru2 = 9,this.hyoujisuru = "")
         currentBattle = new Battle(player, currentEnemy);
       } else if (currentEvent.type == 2) {
         // 魔理沙
         scene = Scenes.Battle;
-        currentEnemy = new BattleCharacter("恐怖マンA", 110, 35, 38, 15, imgMarisa, this.shokin = 30, ageru = 10, this.keikenplus = 45,this.modoru = 10,this.modoru2 = 9)
+        currentEnemy = new BattleCharacter("恐怖マンA", 110, 35, 38, 15, imgMarisa, this.shokin = 30, ageru = 10, this.keikenplus = 45,this.modoru = 10,this.modoru2 = 9,this.hyoujisuru = "")
         currentBattle = new Battle(player, currentEnemy);
       }else if (currentEvent.type == 4) {
         // 魔理沙
         scene = Scenes.Battle;
-        currentEnemy = new BattleCharacter("ERROR", 110, 683468974629784367989743697843743824679866423987463978264397087902634987026379826407962430987246980762409872460978246908724698436987469874369870346987436678436789257864975843987987325498754987532978325978352978975328972853978532978532978359875239785279848795328792538972359789325785329789523789783529785397835279853798253798978235973259797325973597235977352979832597235978235, 38569873296562497869782376983257986325732579869678235968723598623569879867325867235986798632579867325869723586798537698967235986798625398632598625398679687235967235967325697235976852397682359786235987625986732695876982735968325352704370604376807843607843608746370846308764308790347689807946398073469807634089734609874369087436870908743960789436780946370894678467704639807984630798463079864307984609876408976, 0, imgSaikyo, this.shokin = -984366, ageru = 6834689746297843679897436978437438246798664239874639782643970879026349870263798264079624309872469807624098724609782469087268346897462978436798974369784374382467986642398746397826439708790263498702637982640796243098724698076240987246097824690872683468974629784367989743697843743824679866423987463978264397087902634987026379826407962430987246980762409872460978246908726834689746297843679897436978437438246798664239874639782643970879026349870263798264079624309872469807624098724609782469087268346897462978436798974369784374382467986642398746397826439708790263498702637982640796243098724698076240987246097824690872, this.keikenplus = 45,this.modoru = 16,this.modoru2 = 16,this.player.hp = 0,this.player.maxhp = 0, this.player.atc = 0, this.player.speed = -100000, this.player.def = 0, this.name0 = "ERROR",this.spSaikyo.posx = 0,this.spSaikyo.posy = 1)
+        currentEnemy = new BattleCharacter("ERROR", 110, 68346897462978436798974369784374382467986642398746397826439708790263498703646326379826407962430987246980762409872460978246908724698436987469874369870346987436678436789257864975843987987325498754987532978325978352978975328972853978532978532978359875239785279848795328792538972359789325785329789523789783529785397835279853798253798978235973259797325973597235977352979832597235978235, 38569873296562497869782376983257986325732579869678235968723598623569879867325867235986798632579867325869723586798537698967235986798625398632598625398679687235967235967325697235976852397682359786235987625986732695876982735968325352704370604376807843607843608746370846308764308790347689807946398073469807634089734609874369087436870908743960789436780946370894678467704639807984630798463079864307984609876408976, 0, imgSaikyo, this.shokin = -984366, ageru = 68346897462978436798974369784374382467986642398746397826439708790263498702637982640796243098724698076240987246097824690872683468974629784367989743697843743824679866423987463978264397087902634987026379826407962430987246980762409872460978246908726834689746297843679897436978437438246798664239874639782643970879026349870263798264079624309872469807624098724609782469087268346897462978436798974369784374382467986642398746397826439708790263498702637982640796243098724698076240987246097824690872683468974629784367989743697843743824679866423987463978264397087902634987026379826407962430987246980762409872460978483767897890340796807498607948638096408794360794609746307984630978430697809734624690872, this.keikenplus = 45,this.modoru = 26926878936,this.modoru2 = 13486467894367894369874869378966,this.player.hp = 0,this.player.maxhp = 0, this.player.atc = 0, this.player.speed = -100000, this.player.def = 0, this.name0 = "ERROR",this.spSaikyo.posx = 0,this.spSaikyo.posy = 1,this.level = 0,this.keikena = 100,keikenhyouji2 = "経験値.0/0",this.hyoujisuru = "")
         currentBattle = new Battle(player, currentEnemy);
-      }
+        }else if (currentEvent.type == 5) {
+        // 魔理沙
+        scene = Scenes.Battle;
+        currentEnemy = new BattleCharacter("怖いおじさん", 125,45,40, 100000, imgOjisan, this.shokin = 95, ageru = 68, this.keikenplus = 65,this.modoru = 26,this.modoru2 = 20,okorukamo0 = oji0 = 33,okorukamo1 = oji1 = 33,okorukamo2 = spOjisan.posx = 45,okorukamo3 = spOjisan.posy = 45,this.hyoujisuru = "おじさんはどこかに行ってしまった！！")
+        currentBattle = new Battle(player, currentEnemy);
+      }else if (currentEvent.type == 6) {
+        // 魔理沙
+        scene = Scenes.Battle;
+        currentEnemy = new BattleCharacter("メタルニコット", 125,75,65,sensei, imgMetaln, this.shokin = 95, ageru = 68, this.keikenplus = 65,this.modoru = 7,this.modoru2 = 15)
+        currentBattle = new Battle(player, currentEnemy);
+      }else if (currentEvent.type == 7) {
+        // チルノ
+        scene = Scenes.Field;
+        map = map2;
+        spReimu.posx = 17;
+        spReimu.posy = 18;
+        spMarisa.posx = 0;
+        spMarisa.posy = 0;
+        spKyoufu.posx = 0;
+        spKyoufu.posy = 0;
+        spSaikyo.posx = 0;
+        spSaikyo.posy = 0;
+        spCirno.posx = 0;
+        spCirno.posy = 0;
+        spCirno2.posx = 0;
+        spCirno2.posy = 0;
+        spOjisan.posx = 0;
+        spOjisan.posy = 0;
+        spTuyoi.posx = 17;
+        spTuyoi.posy = 15;
+        spMetal.posx = 35;
+        spMetal.posy = 35;
+        spMetal2.posx = 17;
+        spMetal2.posy = 19;
+    }else if (currentEvent.type == 8) {
+      // チルノ
+      scene = Scenes.Field;
+      map = map1;
+      spReimu.posx = 7;
+      spReimu.posy = 15;
+      spMarisa.posx = 14;
+      spMarisa.posy = 14;
+      spKyoufu.posx = 16;
+      spKyoufu.posy = 7;
+      spSaikyo.posx = 1;
+      spSaikyo.posy = 2;
+      spCirno.posx = 10;
+      spCirno.posy = 8;
+      spCirno2.posx = 26;
+      spCirno2.posy = 21;
+      spOjisan.posx = 21;
+      spOjisan.posy = 19;
+      spTuyoi.posx = 35;
+      spTuyoi.posy = 35;
+      spMetal.posx = 7;
+      spMetal.posy = 14;
+      spMetal2.posx = 35;
+      spMetal2.posy = 35;
+    }
   }
   } else if (scene == Scenes.Battle) {
     // 戦闘シーン Enterキーをチェック
@@ -339,12 +507,20 @@ function dispatchEvent(x, y) {
     currentEvent = new GameEvent(0, [kaifukukei]);
   } else if (x == 14 && y == 14) {
     currentEvent = new GameEvent(1, ["恐怖マンBが勝負を仕掛けてきた！！"]);
-  } else if (x == 21 && y == 26) {
+  } else if (x == 26 && y == 21) {
     currentEvent = new GameEvent(3, [kaifukukei]);
   } else if (x == 16 && y == 7) {
     currentEvent = new GameEvent(2, ["恐怖マンAが勝負を仕掛けてきた！！"]);
   }else if (x == 1 && y == 2) {
     currentEvent = new GameEvent(4, ["ERRORが勝負を仕掛けてきた！！"]);
+  }else if (x == oji0 && y == oji1) {
+    currentEvent = new GameEvent(5, ["怖いおじさんがはいよって来た！！"]);
+  }else if (x == 17 && y == 15) {
+    currentEvent = new GameEvent(6, ["メタルニコットが勝負を仕掛けてきた！！"]);
+  }else if (x == 7 && y == 14) {
+    currentEvent = new GameEvent(7,["........"]);
+  }else if (x == 17 && y == 19) {
+    currentEvent = new GameEvent(8,["........"]);
   } else {
     currentEvent = new GameEvent(0, "Error");
   }
@@ -539,20 +715,61 @@ class Battle {
     if (this.enemy.hp <= 0) {
       this.message.push(this.enemy.name + " を倒した！");
       this.message.push("賞金として" + shokin + "ニコットをもらった！");
+      this.message.push(hyoujisuru);
       okane = shokin + okane;
       keiken = keiken + keikenplus;
+      map = map1;      
+      spMarisa.posx = 14;
+      spMarisa.posy = 14;
+      spKyoufu.posx = 16;
+      spKyoufu.posy = 7;
+      spSaikyo.posx = 1;
+      spSaikyo.posy = 2;
+      spCirno.posx = 10;
+      spCirno.posy = 8;
+      spCirno2.posx = 26;
+      spCirno2.posy = 21;
+      spOjisan.posx = 21;
+      spOjisan.posy = 19;
+      spTuyoi.posx = 35;
+      spTuyoi.posy = 35;
       this.status = 1;
+      okorukamo0;
+      okorukamo1;
+      okorukamo2;
+      okorukamo3;
       return;
     } else if (this.player.hp <= 0) {
       this.message.push(this.player.name + " は死んでしまった！");
       this.message.push("賞金として" + ageru + "ニコットを払った！");
       player.hp = player.maxhp;
+      map = map1;
+      spMarisa.posx = 14;
+      spMarisa.posy = 14;
+      spKyoufu.posx = 16;
+      spKyoufu.posy = 7;
+      spSaikyo.posx = 1;
+      spSaikyo.posy = 2;
+      spCirno.posx = 10;
+      spCirno.posy = 8;
+      spCirno2.posx = 26;
+      spCirno2.posy = 21;
+      spOjisan.posx = 21;
+      spOjisan.posy = 19;
+      spTuyoi.posx = 35;
+      spTuyoi.posy = 35;
+      spMetal.posx = 7;
+      spMetal.posy = 14;
+      spMetal2.posx = 35;
+      spMetal2.posy = 35;
       this.status = -1;
       spReimu.posx = modoru;
       spReimu.posy = modoru2;
       okane = okane - ageru;
-      okorukamo0
-      okorukamo1
+      okorukamo4;
+      okorukamo5;
+      okorukamo6;
+      okorukamo7;
       return;
     }
 
